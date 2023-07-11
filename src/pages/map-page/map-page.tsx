9ee0bad7049f8eb8fbda03ef30fe61c9a54686cf
setar92 <tarasiuks2303@gmail.com>
2023-07-11 21:53:08 +0300

    added map page

---
+++ src/pages/map-page/map-page.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { MapComponent } from '../../components';
+
+const MapPage:FC = () => {
+    return <div>
+        <MapComponent></MapComponent>
+    </div>
+}
+
+export { MapPage };
